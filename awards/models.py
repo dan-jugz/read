d7f70b24a1ccf3c1e4a3e39d038af6628ffd00c9
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-21 08:02:32 +0300

    define the post model

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    
+    title=models.CharField(max_length=30)
+    description=models.TextField()
+    link=models.CharField(max_length=100)
+    image=models.ImageField(upload_to='poster/',default='')
+    date_posted=models.DateTimeField(auto_now_add=True)
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
+    design=models.IntegerField(blank=True,default=0)
+    usability=models.IntegerField(blank=True,default=0)
+    creativity=models.IntegerField(blank=True,default=0)
+    content=models.IntegerField(blank=True,default=0)
+    mobile=models.IntegerField(blank=True,default=0)
+    
+    def __str__(self):
+        return f'Post{self.title}--{self.description}--{self.author.username}'
