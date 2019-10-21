b418844e12da25958fd213ca9a7b7b6a130240c4
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-21 08:58:25 +0300

    setup a route and a template that user posts are displayed

--- awards/urls.py
+++ awards/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import (PostCreateView,)
+from .views import (PostCreateView,UserPostListView)
 from . import views
 
 urlpatterns=[
     path('',views.home,name='awards-home'), 
     path('post/new/',PostCreateView.as_view(),name='post-new'),
+    path('user/<str:username>/posts',UserPostListView.as_view(),name='user-posts'),
+    
 ]
