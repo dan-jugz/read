4a212552695ff7d8783231b3ab428fe7f28d5aeb
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-21 08:57:35 +0300

    add class view that renders users posts

--- awards/views.py
+++ awards/views.py
@@ -17,3 +17,20 @@ class PostCreateView(LoginRequiredMixin,CreateView):
     fields=['title','description','link','image'] 
     template_name='awards/post-new.html'  
     success_url='/'
+
+
+# class view to render user posts
+class UserPostListView(ListView):
+    
+    model=Post
+    template_name='awards/user-posts.html'
+    context_object_name='sites'
+    ordering=['-date_posted']
+    paginate_by=5
+
+    def get_queryset(self):
+        '''
+
+        '''
+        user= get_object_or_404(User,username=self.kwargs.get('username'))
+        return Post.get_posts_by_username(user)
