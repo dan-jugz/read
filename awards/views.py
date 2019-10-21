621abe8b22c1eb8fdae99a3f9206963f0e2199b3
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-21 08:44:35 +0300

    create a clas based view that is used to create a post

--- awards/views.py
+++ awards/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import (CreateView)
 
 # Create your views here.
 
 def home(request):
        return render(request,'awards/home.html')
+
+
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    '''
+        using class based view to create a post
+        args:CreateView from django.views.generic
+    ''' 
+    model=Post
+    fields=['title','description','link','image'] 
+    template_name='awards/post-new.html'  
+    success_url='/'
