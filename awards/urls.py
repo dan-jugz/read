8554f5f60d19c388ddc3558a9d001630d87c5db8
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-21 08:45:26 +0300

    setup a route to create neew posts and a template to use

--- awards/urls.py
+++ awards/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import (PostCreateView,)
 from . import views
 
 urlpatterns=[
- path('',views.home,name='awards-home'),   
+    path('',views.home,name='awards-home'), 
+    path('post/new/',PostCreateView.as_view(),name='post-new'),
 ]
