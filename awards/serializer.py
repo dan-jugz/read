58d0b1d10437b395479b7aea686cd2113b7b925f
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-22 18:36:41 +0300

    create  the profile serializer for the purposes of the api calls

---
+++ awards/serializer.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Post
+from users.models import Profile
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Post
+        fields='__all__'
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Profile
+        fields='__all__'     
