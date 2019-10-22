42c216bb9a94c915e21921ab35d56ff5029ca0b0
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-22 11:32:24 +0300

    define a function that registers a user to our website

--- users/views.py
+++ users/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserRegistrationForm,UserUpdateForm,ProfileUpdateForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+# view function for registering 
+def register(request):
+    
+    
+    if request.method=='POST':
+         form=UserRegistrationForm(request.POST)
+
+         if form.is_valid():
+             form.save()
+             username=form.cleaned_data.get('username')
+             messages.success(request,f'Account for {username} created!')
+             return redirect('login')
+
+    else:
+        form=UserRegistrationForm()
+    
+    return render(request,'users/registration.html',{'form':form})
+
 
 # view function for a user profile
 @login_required
