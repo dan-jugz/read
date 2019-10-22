1b68006e93948c3031aa7bc3c7f01159081e03d6
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-22 16:28:16 +0300

    function to create profile once a user is created

---
+++ users/signals.py
@@ -0,0 +1,17 @@
+#signal fired when a user is created
+from django.db.models.signals import post_save
+
+#user to send the signal
+from django.contrib.auth.models import User
+
+#reciever of the signal
+from django.dispatch import receiver
+
+from .models import Profile
+
+
+@receiver(post_save,sender=User)
+def create_profile(sender,instance,created,**kwargs):
+
+    if created:
+        Profile.objects.create(user=instance)
