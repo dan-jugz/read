86f2331fbd3d910aace48ccb5d9414537f6b32da
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-22 09:46:04 +0300

    add the apps to help with the website user functionalities

--- awwwardz/settings.py
+++ awwwardz/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'awards'
+    'awards',
+    'crispy_forms',
+    'bootstrap4',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +119,23 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Directory to save our uploaded files
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/'
+
+
+CRISPY_TEMPLATE_PACK='bootstrap4'
+
+# hijacking django and redirecting user to the homepage
+LOGIN_REDIRECT_URL='insta-home'
+LOGIN_URL='login'
