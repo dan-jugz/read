1e834deea758003839e8bcb95e02d79fc5855ad5
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-22 12:32:29 +0300

    set the route to the registering and profile templates

--- awwwardz/urls.py
+++ awwwardz/urls.py
@@ -17,9 +17,16 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path,include
 
+from users import views as user_views
+# setting for the images path
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('awards.urls')),
+    path('accounts/register/',user_views.register,name='register'),
+    path('accounts/profile/',user_views.profile,name='profile'),
 ]
 if settings.DEBUG:
 
