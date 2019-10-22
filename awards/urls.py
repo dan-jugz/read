c2cdb7f750371e6a9eafd167a5a6825ce3cc7948
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-22 10:43:04 +0300

    add the route to display searched query in the urls

--- awards/urls.py
+++ awards/urls.py
@@ -9,5 +9,6 @@ urlpatterns=[
     path('post/<int:pk>/',views.postDetail,name='post-detail'),
     path('post/<int:pk>/update/',PostUpdateView.as_view(),name='post-update'),
     path('post/<int:pk>/delete/',PostDeleteView.as_view(),name='post-delete'),
+    path('search/',views.search_results,name='search-results'),
     
 ]
