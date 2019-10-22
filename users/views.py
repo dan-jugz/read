dff2b970f4277cc5550f6809f484b32ef6f9217b
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-22 11:25:44 +0300

    define a function that renders the user profile page

--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# view function for a user profile
+@login_required
+def profile(request):
+    
+    
+
+    user=User.objects.get(pk=request.user.id)
+    posts=user.post_set.all()
+    context={
+        'usr_form':usr_form,
+        'prof_form':prof_form,
+        'posts':posts
+    }
+
+    
+
+    return render(request,'users/profile.html',context)
