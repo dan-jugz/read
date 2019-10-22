d4aa167a4cd9d753ec5340a102d170caa3bf2124
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-22 11:15:04 +0300

    add a profile model to the user app

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
+
+class Profile(models.Model):
+    
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    bio=models.TextField(max_length=140,blank=True)
+    profile_photo=models.ImageField(upload_to='profile_pics',default='default_profile.png',blank=True)
+    
+    def __str__(self):
+        return f'{self.user.username}-Profile'
