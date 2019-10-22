75ed9f3bbf8c030dcda50fba197697591af5d5d7
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-22 11:28:36 +0300

    populate the profile fields according to the user that is logged in

--- users/views.py
+++ users/views.py
@@ -7,6 +7,20 @@ from django.shortcuts import render
 def profile(request):
     
     
+    # instance=request.user populating the fields with the current user details
+    if request.method=='POST':
+        usr_form=UserUpdateForm(request.POST,instance=request.user)
+        prof_form=ProfileUpdateForm(request.POST,request.FILES,instance=request.user.profile)
+        
+        if usr_form.is_valid and prof_form.is_valid:
+            usr_form.save()
+            prof_form.save()
+            messages.success(request,f'Your Account Has Been Updated')
+
+            return redirect('profile')
+    else:
+        usr_form=UserUpdateForm(instance=request.user)
+        prof_form=ProfileUpdateForm(instance=request.user.profile)
 
     user=User.objects.get(pk=request.user.id)
     posts=user.post_set.all()
