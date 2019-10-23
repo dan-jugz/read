145f273683b91da0af4ae573735fe7a14bd2dbfb
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-23 17:47:28 +0300

    changed database settings

--- awwwardz/settings.py
+++ awwwardz/settings.py
@@ -36,26 +36,13 @@ ALLOWED_HOSTS = []
 MODE=config("MODE", default="dev")
 
 DEBUG = config('DEBUG', default=False, cast=bool)
-# development
-if config('MODE')=="dev":
-   DATABASES = {
-       'default': {
-           'ENGINE': 'django.db.backends.postgresql_psycopg2',
-           'NAME': config('DB_NAME'),
-           'USER': config('DB_USER'),
-           'PASSWORD': config('DB_PASSWORD'),
-           'HOST': config('DB_HOST'),
-           'PORT': '',
-       }
-       
-   }
-# production
-else:
-   DATABASES = {
-       'default': dj_database_url.config(
-           default=config('DATABASE_URL')
-       )
-   }
+
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
 
 db_env = dj_database_url.config(conn_max_age=500)  
 DATABASES['default'].update(db_env)  
